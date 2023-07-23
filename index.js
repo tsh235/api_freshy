14a589b94a5124d03c623796cb7a12ff36327fd3
Tatyana <85416087+tsh235@users.noreply.github.com>
2023-07-23 23:21:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
